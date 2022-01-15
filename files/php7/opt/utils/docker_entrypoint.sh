49a43d19d4ad9b4d6bc553bec98fc7dcdf42b111
Loi Tra 9thwonder <trathailoi@gmail.com>
2022-01-15 20:42:35 +0700

    colorize output

--- files/php7/opt/utils/docker_entrypoint.sh
+++ files/php7/opt/utils/docker_entrypoint.sh
@@ -1,39 +1,42 @@
 #!/bin/env bash
+color_yellow_with_bg='\e[0;93;100m'
+# Clear the color after that
+color_clear='\033[0m \n\n'
 
 if [ -z "${EXTRA_PACKAGES}" ]; then
-    echo "\e[1;93;100m No extra packages to install. \e[0m"
+    printf "${color_yellow_with_bg} No extra packages to install. ${color_clear}"
 else
-    echo "\e[1;93;100m Installing extra packages... \e[0m"
+    printf "${color_yellow_with_bg} Installing extra packages... ${color_clear}"
     apk -U --no-cache add ${EXTRA_PACKAGES}
 fi
 
 if [ -z "${IS_WORDPRESS}" ]; then
-    echo "\e[1;93;100m Not a Wordpress site. No need to instal WP-CLI and packages. \e[0m"
+    printf "${color_yellow_with_bg} Not a Wordpress site. No need to instal WP-CLI and packages. ${color_clear}"
 else
-    echo "\e[1;93;100m Install PHP extensions - for WP-CLI \e[0m"
+    printf "${color_yellow_with_bg} Install PHP extensions - for WP-CLI ${color_clear}"
     apk -U --no-cache add \
         php7-phar \
         php7-json \
         php7-mysqli \
         php7-zip
 
-    echo "\e[1;93;100m Install WP-CLI \e[0m"
+    printf "${color_yellow_with_bg} Install WP-CLI ${color_clear}"
     # curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
     wget -P /tmp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
     chmod o+x /tmp/wp-cli.phar; chmod 755 /tmp/wp-cli.phar;
     mv /tmp/wp-cli.phar /usr/local/bin/wp;
 
-    echo "\e[1;93;100m Begin Download WP Core \e[0m"
+    printf "${color_yellow_with_bg} Begin Download WP Core ${color_clear}"
     wp core download --skip-content --allow-root --path=/var/www/html
 
-    echo "\e[1;93;100m Correct permissions for UPLOADS folder \e[0m"
+    printf "${color_yellow_with_bg} Correct permissions for UPLOADS folder ${color_clear}"
     mkdir -p /var/www/html/wp-content/uploads && mkdir -p /var/www/html/wp-content/uploads/cache
     chmod -R 777 /var/www/html/wp-content/uploads
 
     if [ -z "${HAS_COMPOSER}" ]; then
-        echo "\e[1;93;100m No Composer. \e[0m"
+        printf "${color_yellow_with_bg} No Composer. ${color_clear}"
     else
-        echo "\e[1;93;100m Install PHP extensions - for Composer \e[0m"
+        printf "${color_yellow_with_bg} Install PHP extensions - for Composer ${color_clear}"
         apk -U --no-cache add \
             php7-bcmath \
             php7-bz2 \
@@ -55,17 +58,17 @@ else
             php7-tokenizer \
             php7-xmlwriter
 
-        echo "\e[1;93;100m Install Composer \e[0m"
+        printf "${color_yellow_with_bg} Install Composer ${color_clear}"
         # php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/tmp/ --filename=composer; alias composer='php /tmp/composer';
         php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/local/bin/ --filename=composer;
 
-        echo "\e[1;93;100m Begin composer install \e[0m"
+        printf "${color_yellow_with_bg} Begin composer install ${color_clear}"
         # composer install --ignore-platform-reqs --working-dir=/var/www/html/wp-content/themes/$THEME_NAME/
         composer install --working-dir=/var/www/html/wp-content/themes/$THEME_NAME/
     fi
 
     # Generate wp-config.php file
-    echo "\e[1;93;100m Generate wp-config.php file \e[0m"
+    printf "${color_yellow_with_bg} Generate wp-config.php file ${color_clear}"
     awk '
             /put your unique phrase here/ {
                     cmd = "head -c1m /dev/urandom | sha1sum | cut -d\\  -f1"
